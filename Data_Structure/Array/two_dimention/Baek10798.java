4a4790caf5050acb785872ccb732b0a9c8352ab4
leeminjxun <th554751@naver.com>
2025-05-10 01:02:50 +0900

    ✅ 25-05-10
    백준 2차원 배열 문제 : 10798, 2563

--- Data_Structure/Array/two_dimention/Baek10798.java
+++ Data_Structure/Array/two_dimention/Baek10798.java
@@ -7,8 +7,34 @@ package Data_Structure.Array.two_dimention;
 // 주어지는 글자는 영어 대문자 'A'부터 'Z', 영어 소문자 'a' 부터 'z', 숫자 '0'부터 '9' 까지 중 하나이다.
 // 각 줄의 시작과 마지막에 빈칸은 없다.
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Baek10798 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] arr = new char[5][15];
+
+        int max = 0;
+
+        for (int i = 0; i < 5; i++){
+            String str = br.readLine(); // 한 행에 공백 없이 입력을 받으므로 br 객체를 사용하는 것이 올바르다.
+
+            if(str.length() > max) max = str.length(); // max 크기는 입력을 받은 후 정해진다.
+
+            for (int j = 0; j < str.length(); j++){
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < max; i++){
+            for (int j = 0; j < 5; j++){
+                if (arr[j][i] == '\0') continue;
+
+                System.out.print(arr[j][i]);
+            }
+        }
 
     }
 }
---
+++ Data_Structure/Array/two_dimention/Baek2563.java
@@ -0,0 +1,60 @@
+package Data_Structure.Array.two_dimention;
+
+// 백준 [2563] 문제 유형 = 2차원 배열
+
+// 문제 :
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek2563 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[100][100];
+        int area = 0;
+
+        // [ 풀이 1 : int 형 2차원 배열 ]
+        /*for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+
+            for (int j = y; j < (y + 10); j++){
+                for (int k = x; k < (x + 10); k++) arr[j][k] = 1;
+            }
+        }
+
+        for (int i = 0; i < 100; i++){
+            for (int j = 0; j < 100; j++){
+                if (arr[i][j] == 1) area++;
+            }
+        }*/
+
+        // [ 풀이 2 : boolean 형 2차원 배열 ]
+        boolean[][] paper = new boolean[100][100];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+
+            for (int j = y; j < (y + 10); j++){
+                for (int k = x; k < (x + 10); k++) {
+                    if (!paper[j][k]){
+                        paper[j][k] = true;
+                        area++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(area);
+    }
+}
