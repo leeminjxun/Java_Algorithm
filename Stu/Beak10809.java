b6d1abf48eecb9b0c58ee01734cb0ca5973b36cf
leeminjxun <th554751@naver.com>
2025-09-17 15:50:25 +0900

    ✅ 2025-09-17
    Baekjoon 10809

---
+++ Stu/Beak10809.java
@@ -0,0 +1,33 @@
+package Stu;
+
+import java.util.*;
+import java.io.*;
+
+public class Beak10809 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 알파벳 위치를 나타내는 배열
+        int[] arr = new int[26];
+
+        int index = 0;
+
+        String alpha = br.readLine();
+
+        Arrays.fill(arr, -1);
+
+        for (int i = 0; i < alpha.length(); i++) {
+
+            index = (int)alpha.charAt(i);
+
+            if (arr[index - 97] == -1) arr[index - 97] = i;
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
