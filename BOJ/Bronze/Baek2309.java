5326b43929fe7f091328f8fa01f6053a4d506d65
leeminjxun <th554751@naver.com>
2025-11-14 12:58:13 +0900

    25-11-14

---
+++ BOJ/Bronze/Baek2309.java
@@ -0,0 +1,34 @@
+package BOJ.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class Baek2309 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    Arrays.sort(arr);
+
+                    for(int k = 2; k < 9; k++) {
+                        System.out.println(arr[k]);
+                    }
+
+                    return;
+                }
+            }
+        }
+    }
+}
